d70267bd4f94e68489d99b6548c76900f0f47cae
j-s-n <jasonkuroda@gmail.com>
2024-01-11 15:20:30 -0500

    rendered home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const App = () => {
   return (
-    <h1> 
-      Hello World!
-    </h1>
+    <div>
+      <h1> 
+        Hello World!
+      </h1>
+    </div>
   )
 }
 
