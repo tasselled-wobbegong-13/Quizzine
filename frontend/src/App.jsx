961fa49fae54a58f2d7cde2e9377d37355411d73
j-s-n <jasonkuroda@gmail.com>
2024-01-12 15:53:25 -0500

    started react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Dashboard from "./views/Dashboard";
-import NewEvent from "./views/NewEvent";
+import Home from "./views/Home"; 
+import NewEvent from "./views/NewEvent"; 
+import Error from "./views/Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-      <NewEvent />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="add-event" element={<NewEvent />}/>
+      <Route path="*" element={<Error />}/>
+    </Routes>
+  </BrowserRouter>
   )
-}
+
+}  
 
 export default App;
