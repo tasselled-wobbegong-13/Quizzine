0f5d3692b527fa6306282bafaa5bc41f503d9a4f
j-s-n <jasonkuroda@gmail.com>
2024-01-12 17:07:38 -0500

    NavBar progress

---
+++ frontend/src/components/PopulateUsers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PopulateUsers = ({users}) => {
+  console.log('users in populate users', users)
+  return users.map((user, index) => {
+    return (
+      <li key={index}>
+        {user}
+      </li>
+    )
+  })
+}
+
+export default PopulateUsers;
