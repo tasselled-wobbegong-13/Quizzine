cad2047a4536ebef7fbbd09fd41b4437f6023354
j-s-n <jasonkuroda@gmail.com>
2024-01-17 15:03:31 -0500

    delete user from group invite

--- frontend/src/components/PopulateUsers.jsx
+++ frontend/src/components/PopulateUsers.jsx
@@ -3,18 +3,20 @@ import React from "react";
 const PopulateUsers = ({users, setUsers}) => {
   console.log(users)
 
-  const handleOnClick = async (e) => {
-    console.log('hello')
-    console.log(e.target)
+  const deleteOnClick = async (e) => {
+
+    const userEmail = e.target.value
+
+    setUsers((users) => users.filter((user, index) => user !== userEmail) );
   }
 
   return users.map((user, index) => {
     return (
-      <div className="userListContainer" key={index} >
+      <div className="userListContainer" >
         <ul className="unorderedList">
-          <li className="listItemInUserListContainer">
+          <li className="listItemInUserListContainer" key={index}>
             {user}
-            <button className="buttonInListItem" onClick={handleOnClick}> X </button>
+            <button className="buttonInListItem" onClick={deleteOnClick} value={user}> X </button>
           </li>
           </ul>
       </div>
