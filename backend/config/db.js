cfded2ff42be6163abc5883eafddf2886190ce50
j-s-n <jasonkuroda@gmail.com>
2024-01-17 19:17:51 -0500

    add event detail to user

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,16 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       //Aaron's Mongo URI:
-      'mongodb+srv://yelpScratchProject:airplanefox-456@cluster0.4mb5rwm.mongodb.net/'
+      // 'mongodb+srv://yelpScratchProject:airplanefox-456@cluster0.4mb5rwm.mongodb.net/'
 
       //Ando's Mongo URI:
       //'mongodb+srv://arivreduce:g9pOctOjyaU8DtYh@quizzine.pnx3hgq.mongodb.net/?retryWrites=true&w=majority'
 
       //Jason's Mongo URI:
+      'mongodb+srv://jasonkuroda:mongo@cluster0.ngx3ozr.mongodb.net/?retryWrites=true&w=majority'
+
+      //Daniel's Mongo URI:
+      // 'mongodb+srv://sindaniel:h1JbOxNssdjuw6zA@cluster0.vvtsenh.mongodb.net/?retryWrites=true&w=majority'
 
 
     );
