4dc7b9e46c44a1f768a785df9b65ecfcff4936c4
Andranik Agamalian <128445370+andranik-agamalian@users.noreply.github.com>
2024-06-02 12:25:36 -0700

    Update yelpApiController.js

--- backend/controllers/yelpApiController.js
+++ backend/controllers/yelpApiController.js
@@ -1,6 +1,6 @@
 import CreatedNewEvent from '../models/createEventModel.js';
 import yelp from 'yelp-fusion';
-const apiKey = 'ItLTRWLz_FY3ADZur2BLEUhRmBAWcb_Zqaszn958CQj-a_0SV4VDX9ODpvKzh2WcHSjf_gCH5jNpilDLUBpojCO80oJXhesmFC6Q2ROB_wDnSeiEA2iq5B0ZRTmnZXYx';
+const apiKey = 'OBTAIN API KEY FROM YELP API FUSION';
 const client = yelp.client(apiKey);
 
 
